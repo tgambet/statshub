32998d9f4ab4495913c1488d62017b0f79f5740f
Thomas Gambet <thomas.gambet@gmail.com>
2019-06-17 21:45:35 +0200

    flattened app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,14 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+    <charts4ng-compo></charts4ng-compo>
+    <router-outlet></router-outlet>
+  `,
+  styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
