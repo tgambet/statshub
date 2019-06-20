a561f79d3fd883acc53b6feb84973d246e9cffca
Thomas Gambet <thomas.gambet@gmail.com>
2019-06-20 16:34:03 +0200

    added graphql-codegen tool and generated github schema

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,42 +2,32 @@ import {Injectable} from '@angular/core';
 import {asapScheduler, defer, Observable, of, scheduled, throwError} from 'rxjs';
 import {filter, mergeMap, switchMap, tap} from 'rxjs/operators';
 import {Apollo} from 'apollo-angular';
-import gql from 'graphql-tag';
-
-export const LOGIN_QUERY = gql`
-  query {
-    viewer {
-      name
-    }
-  }
-`;
+import {User, ViewerGQL} from '../github.schema';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  user: any;
+  user: Pick<User, 'name' | 'login'>;
 
-  constructor(private apollo: Apollo) { }
+  constructor(private apollo: Apollo, private viewerGQL: ViewerGQL) { }
 
   isLoggedIn(): boolean {
     return !!this.user;
   }
 
-  login(token: string): Observable<any> {
+  login(token: string): Observable<Pick<User, 'name' | 'login'>> {
     const setToken = defer(() =>
       scheduled([localStorage.setItem('token', token)], asapScheduler)
     );
-    const login = this.apollo.query({
-      query: LOGIN_QUERY
-    }).pipe(
+    const login = this.viewerGQL.fetch().pipe(
       filter(result => !result.loading),
       switchMap((result) => {
         if (result.errors) {
           return throwError(result.errors);
         } else {
-          return of(result.data);
+          return of(result.data.viewer);
         }
       }),
       tap(user => this.user = user)
